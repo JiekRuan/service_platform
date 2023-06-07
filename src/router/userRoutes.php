f2f336f22ee24af759c3e450233af16eb7d70e82
Hugo Cieplucha <cieplucha.hugo@gmail.com>
2023-06-07 14:34:30 +0200

    startet userRoutes

---
+++ src/router/userRoutes.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once './controllers/userController.php';
+
+$method = $_SERVER['REQUEST METHOD'];
+$url = $_SERVER['REQUEST URI'];
+
+switch ($url) {
+    case 'value':
+        # code...
+        break;
+    
+    default:
+        # code...
+        break;
+}
