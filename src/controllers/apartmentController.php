fadfcdfd3b38607968466c22a4c087d757d7048b
fouuuadi <fouadlamnaouar18@gmail.com>
2023-06-11 17:31:49 +0200

    changement apartment -> apartmentModel,  debut du apartmentController et création apartmentRoutes

---
+++ src/controllers/apartmentController.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'Apartment.php';
+
+class ApartmentController {
+    public function addNewApartment($data) {
+        $apartment = new Apartment(null, $data['name'], $data['capacity'], $data['description'], $data['price'], $data['location']);
+        
+    }
+
+    public function deleteApartment($id) {
+        $apartment = new Apartment($id, null, null, null, null, null);
+        
+    }
+
+    public function modifyApartment($data) {
+        $apartment = new Apartment(null, $data['name'], $data['capacity'], $data['description'], $data['price'], $data['location']);
+        
+    }
+}
