5d18f0a22bc980cc3b27cbcc0c70712f31d2db4c
Hugo Cieplucha <cieplucha.hugo@gmail.com>
2023-06-12 15:40:03 +0200

    ajout fichier json pour les routes

--- src/router/router.php
+++ src/router/router.php
@@ -23,22 +23,26 @@ class Router {
         $this->domain = preg_split('/\//', $this->url)[0];
 
         //  on récupère le reste de la requêta après le nom de domaine
-        $this->route = preg_replace('/^' . $this->domain . '\//', '$0 --> $2 $1', $this->url);
+        $this->route = preg_replace('/^' . $this->domain . '/', '$0 --> $2 $1', $this->url);
+
+        if($this->route[0] == '/'){
+            array_shift($this->route);
+        }
     }
 
 
     //  routage
     public function dispatch() {
 
-        if($this->route == null) {
+        //je récupère les routes
+        $routes = $this->getRoutes();
+
+        if($routes == []) {
             //  require la homepage
             //
             //
             return true;
         }
-
-        //je récupère les routes
-        $routes = $this->getRoutes();
 
         foreach($routes as $route) {
             if($route['url'] == $this->route){
@@ -48,21 +52,8 @@ class Router {
     }
 
     //  fonction qui récupère les routes concernées par la requête
+    //  faire un json pour stocker toutes les routes
     public function getRoutes(){
 
-        switch ($this->route[0]) {
-            case 'users':
-                //  retourne la liste des routes utilisateurs
-                break; 
-            case 'apartment':
-                //  retourne la liste des routes utilisateurs
-                break;
-            case 'reservation':
-                //  retourne la liste des routes utilisateurs
-                break;
-            default:
-                return [];
-                break;
-        }
     }
 }
