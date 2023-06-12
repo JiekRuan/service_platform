735f25dd55f1773222587742871a1af9c03c0575
Linelinlove <chrisline.lin@outlook.fr>
2023-06-12 15:13:47 +0200

    fix

--- public/templates/public/settings.php
+++ public/templates/public/settings.php
@@ -6,9 +6,11 @@
 
 <div class="superContainer">
     <div class="Container">
-        <h1>PARAMÈTRES</h1>
-        <p class="descritionParametre">Votre vie privée est notre priorité, et nous veillons à ce que toutes les 
-            mesures de sécurité appropriées soient en place pour garantir la protection de vos données confidentielles.</p>
+        <h2>PARAMÈTRES</h2>
+        <p class="descritionParametre">
+            Votre vie privée est notre priorité, et nous veillons à ce que toutes les
+            mesures de sécurité appropriées soient en place pour garantir la protection de vos données confidentielles.
+        </p>
 
         <form class="infoForm">
             <div class="radiobox">
@@ -32,7 +34,7 @@
                 <input name="email" type="email" class="inputText" placeholder="Email" required>
             </div>
             <input type="submit" class="goldenButton" value="Mettre à jour">
-                    
+
         </form>
     </div>
 </div>
@@ -40,8 +42,8 @@
 
 <div class="superContainer">
     <div class="Container">
-        <h1>MODIFIER MOT DE PASSE</h1>
-        
+        <h2>MODIFIER MOT DE PASSE</h2>
+
         <form class="infoForm">
             <div class="row">
                 <input name="password" type="password" class="inputText" placeholder="Mot de passe" required>
