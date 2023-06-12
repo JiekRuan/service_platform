3289fdbbd1030fb168f615162c09842b04a6552c
Nyoote <120457719+Nyoote@users.noreply.github.com>
2023-06-12 14:46:41 +0200

    pages connexion, inscription, parametres et css

---
+++ public/templates/public/login.php
@@ -0,0 +1,24 @@
+<?php include '../componant/header.html' ?>
+<link rel="stylesheet" href="../../css/loginSignupParametre.css">
+<link rel="stylesheet" href="../../css/global.css">
+
+
+
+    <div class="superContainer">
+        <div class="Container">
+            <h1>CONNEXION</h1>
+
+            <form class="infoForm">
+                    <div class="row">
+                        <input name="name" type="text" class="inputText" placeholder="Email" required>
+                    </div>
+                    <div class="row">
+                        <input name="password" type="password" class="inputText" placeholder="Mot de passe" required>
+                    </div>
+                    
+                    <input type="submit" class="goldenButton" value="Se connecter">
+                    
+            </form>
+        </div>
+        
+    </div>
