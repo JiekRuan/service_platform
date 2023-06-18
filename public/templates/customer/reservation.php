7b7fbbf4320966e460f2564498a6d12dd6de8b5e
Linelinlove <chrisline.lin@outlook.fr>
2023-06-18 16:39:33 +0200

    fix page

--- public/templates/customer/reservation.php
+++ public/templates/customer/reservation.php
@@ -1,83 +1,50 @@
-<!DOCTYPE html>
-<html lang="fr">
+<?php include '../componant/header.html' ?>
+<link rel="stylesheet" href="../../css/searchPage.css">
+<link rel="stylesheet" href="../../css/logement.css">
+<link rel="stylesheet" href="../../css/reservation.css">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../css/global.css">
-    <link rel="stylesheet" href="../../css/header.css">
-    <!-- <link rel="stylesheet" href="../../css/footer.css"> -->
-    <link rel="stylesheet" href="../../css/logement.css">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-    <link rel="icon" type="image/png" href="../../images/logo.png">
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/brands.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-
-    <title>Client détail réservation</title>
-</head>
-
-<body>
-    <header>
-        <h3 class="logo">Elysians Paris</h3>
-        <nav>
-            <ul id="desktop">
-                <li id="search"><i class="fa-solid fa-magnifying-glass"></i></li>
-                <li id="searchForm">
-                    <form action="" method="GET">
-                        <input class="searchInput" type="text" placeholder="16ème arrondissement...">
-                    </form>
-                </li>
-                <li><i class="fa-regular fa-bookmark"></i></li>
-                <li><i class="fa-regular fa-message"></i></li>
-                <li><i class="fa-regular fa-user"></i></li>
-            </ul>
-            <ul id="mobile">
-                <li id="searchMobile"><i class="fa-solid fa-magnifying-glass"></i></li>
-                <li id="searchFormMobile">
-                    <form action="" method="GET">
-                        <input class="searchInput" type="text" placeholder="16ème arrondissement...">
-                    </form>
-                </li>
-                <li id="headerBurger"><i class="fa-solid fa-bars"></i></li>
-            </ul>
-            <ul id="headerBurgerContent">
-                <li><i class="fa-regular fa-bookmark"></i><p>Vos favoris</p></li>
-                <li><i class="fa-regular fa-message"></i><p>Messagerie</p></li>
-                <li><i class="fa-regular fa-user"></i><p>Votre Profil</p></li>
-            </ul>
-        </nav>
-    </header>
 
 <main>
-    <section class="nav-bar">
-        <h1>
-            Votre réservation
-        </h1>
-        <p>Bonjour [nom d’utilisateur], nous avons le plaisir de vous accueillir chez nous.Voici le détail concernant votre réservation au sein de notre établissement.</p>
-    </section>
+    <div class="banner">
+        <h1>Votre réservation</h1>
+        <p>Bonjour [nom d'utilisateur], nous avons le plaisir de vous accueillir chez nous. Voici le détail concernant votre réservation au sein de notre établissement.</p>
+    </div>
 
     <section class="container">
         <h1>Appartement exceptionnel 4 pièces au Trocadéro</h1>
 
-        <article>
-            <h2>Description</h2>
-            <p class="info">300m2   |   5 chambres   |   4 salles de bain</p>
+        <article class="containerContent">
+            <div class="descriptionBookmark">
+                <h2>Description</h2>
+                <i class="fa-regular fa-bookmark"></i>
+            </div>
+            <p class="info">300m2 | 5 chambres | 4 salles de bain</p>
 
-            <img src="../../images/troca.png" alt="appartement 4 pièces">
+            <figure class="containerImage">
+                <img src="../../images/troca.png" alt="appartement 4 pièces">
+            </figure>
+
+            <section class="galleryContainer">
+                <article class="gallery">
+
+                    <div class="galerie">
+                        <figure>
+                            <img src="../../images/sdb.png" alt="salle de bain">
+                        </figure>
+                        <figure>
+                            <img src="../../images/salon.png" alt="salon">
+                        </figure>
+                        <figure>
+                            <img src="../../images/cuisine.png" alt="cuisine">
+                        </figure>
+                    </div>
+                </article>
+            </section>
 
             <div class="text-describe">
-                <div class="galerie">
-                    <img src="../../images/sdb.png" alt="salle de bain">
-                    <img src="../../images/salon.png" alt="salon">
-                    <img src="../../images/cuisine.png" alt="cuisine">
-                    </div>
+                <p>Cette superbe proriété de quatre chambres est l’expression ultime de l’architecture contemporaine, offrant à ses résidents un décor inpeccable dans une palette de couleurs élégantes pour créer une atmosphère luxieuse et rayonante.</p>
+                <p>La propriété comprend une cuisine ultra moderne et trois grands espaces de vie avec des baies vitrées, permettant à la lumière naturelle d’inondée l’espace. </p>
+                <p>Les équipements du batiment sont conçus pour une expérience clé en main, offrant au résident un parking privé, une cave, une conciergerie, une sécurité 24h/24 et une piscine olympique. De plus, la terrasse vous offrira un cadre sublime pour vous imprénier du paysage et d’un soleil radieux.</p>
             </div>
         </article>
     </section>
@@ -85,98 +52,90 @@
     <section>
         <article class="features">
             <div class="caracteristic">
-                <h2>Caractéristiques</h2> <hr>
-                <p>100m2</p> <hr>
-                <p>1 jacuzzi</p> <hr>
-                <p>5 chambres</p> <hr>
+                <h2>Caractéristiques</h2>
+                <hr>
+                <p>100m2</p>
+                <hr>
+                <p>1 jacuzzi</p>
+                <hr>
+                <p>5 chambres</p>
+                <hr>
             </div>
             <div class="caracteristic">
-                <h2>Agréments</h2> <hr>
-                <p>Vue sur la mer</p> <hr>
-                <p>terrasse</p> <hr>
+                <h2>Agréments</h2>
+                <hr>
+                <p>Vue sur la mer</p>
+                <hr>
+                <p>terrasse</p>
+                <hr>
             </div>
             <div class="caracteristic">
-                <h2>Particularités</h2> <hr>
-                <p class="carac-text">Situé au 28e étage d’une résidence exclusive les appartements du chateau du Périgord ont été magnifiquement conçu dans un esprit de luxe et de douceur de vivre. En tant que summum du paysage architectural monégasque, les résidents bénéficient d’une vue panorama sur Monaco et son paysage azur méditeranéen qui s’étend à perte de vue.</p>
-            </div>
-        </article>
-    </section> 
-
-    <section class="">
-       
-
-        <article class="further">
-            <h1>Services</h1>
-            <div >
-                
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fringilla ipsum a odio lacinia, eu condimentum sem ultrices. 
-
-                    Pellentesque eleifend elit nunc, id commodo nisl sollicitudin sit amet. Sed efficitur, tortor non dapibus varius, sapien odio faucibus lacus, 
-                    
-                    ac malesuada tellus nulla ac ligula. Proin auctor condimentum odio in porta. Nam ut efficitur purus, id gravida lectus. 
-                    Duis consectetur faucibus felis, a ullamcorper metus rhoncus a. Donec interdum mi id magna eleifend, eget commodo orci pharetra. Nulla facilisi.
-                    Praesent id tortor auctor, finibus massa a, faucibus nisl. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
-                
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium magni deleniti saepe dolorum excepturi quidem earum natus veniam, eveniet, neque itaque fugit ratione at molestias rem? Mollitia explicabo nesciunt voluptate.
-
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto accusantium ipsum quas molestiae assumenda dicta a doloribus, facere quia quo voluptates temporibus magni, neque pariatur perferendis amet tempora cupiditate aperiam?
-
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, accusamus quam odio atque tempore fuga, iusto similique, quis ipsam praesentium consequatur! Ab voluptatibus quibusdam vitae culpa! Pariatur labore obcaecati atque!
-
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae ex nemo qui ad, natus consequuntur voluptates iusto, sequi enim aspernatur distinctio autem? Reiciendis eum cum ad aperiam! Saepe, aperiam qui.
-                    
-                    <div><button class="add">Ajouter une note</button></div>
-                </p>
-
-               
+                <h2>Particularités</h2>
+                <hr>
+                <p class="carac-text">Situé au 28e étage d'une résidence exclusive les appartements du chateau du Périgord ont été magnifiquement conçu dans un esprit de luxe et de douceur de vivre. En tant que summum du paysage architectural monégasque, les résidents bénéficient d’une vue panorama sur Monaco et son paysage azur méditeranéen qui s’étend à perte de vue.</p>
             </div>
         </article>
     </section>
 
-    <section class="">
-       
-
+    <section>
         <article class="further">
-            <h1>Renseignement</h1>
-            <div >
-                <p class="further-info"><img src="../../images/agence.png" alt=""></p>
-                <p>Pour toute information complémentaire nous restons à votre entière disposition et serons ravis de pouvoir vous aider. Le service client est disponible 24h/24 toute la semaine via téléphone, whatsapp, email ou en agence.</p>
+            <h2>Vos services</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam maiores corporis quaerat aspernatur aut ducimus, nemo iste autem eum, pariatur sint nam necessitatibus iure deleniti. Voluptate est aspernatur quam adipisci.</p>
 
-                <article class="further-footer">
-                    <div>
-                        <h3>Téléphone</h3> <hr>
-                        <p>+33 6 59 38 53 35</p>
-                    </div>
-                    <div>
-                        <h3>Email</h3> <hr>
-                        <p>contact.elysians@gmail.com</p>
-                    </div>
-                    <div>
-                        <h3>Agence</h3> <hr>
-                        <p>27 bis rue du Progrès, Paris 75016</p>
-                    </div>
-                
-                </article>
+            <div class="updateService">
+                <p class="blueButton" id="editButton">Modifier vos services</p>
+                <p class="nb">Notez que des services supplémentaires engendreront des frais supplémentaires. Contactez-nous pour plus d'informations.</p>
             </div>
         </article>
     </section>
 
-    
 
-    
-    <div class="btn" ><button class="add">Annuler la réservation</button></div>
+    <section>
+        <article class="further">
+            <h2>Renseignement</h2>
+            <div class="logementInfo">
+                <div class="logementMoreInfo">
+                    <p>Pour toute information complémentaire nous restons à votre entière disposition et serons ravis de pouvoir vous aider. Le service client est disponible 24h/24 toute la semaine via téléphone, whatsapp, email ou en agence.</p>
+
+                    <article class="further-footer">
+                        <div>
+                            <h4>Téléphone</h4>
+                            <hr>
+                            <p>+33 6 59 38 53 35</p>
+                        </div>
+                        <div>
+                            <h4>E-mail</h4>
+                            <hr>
+                            <p>elysians_paris@hotmail.com</p>
+                        </div>
+                        <div>
+                            <h4>Agence</h4>
+                            <hr>
+                            <p>27 bis rue du Progrès, Paris 75016</p>
+                        </div>
+                    </article>
+                </div>
+                <figure class="logementInfoImage">
+                    <img src="../../images/agence.png" alt="">
+                </figure>
+            </div>
+        </article>
+    </section>
+
+    <p class="goldenButton" id="cancel">Annuler la réservation</p>
+
+    <div id="cancelReservation">
+        <p>Êtes-vous sûr(e) de vouloir annuler cette réservation ?</p>
+        <div id="readDeleteMenuButton">
+            <div class="userForm">
+                <p class="blueButton cancelDesactivate">Retour</p>
+            </div>
+            <form action="" method="POST"><input type="submit" value="Confirmer" class="goldenButton"></form>
+        </div>
+    </div>
+
 </main>
 
+<script src="../../js/reservation.js"></script>
 
-
-
-<footer>
-   
-   
-</footer>
-
-
-
-</body>
-    <script src="../../js/app.js"></script>
-    <script src="../../js/header.js"></script>
+<?php include '../componant/footer.html' ?>
