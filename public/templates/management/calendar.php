8443cd57c50bdf4c34ca07d6395380ecaae17605
Jonathan Luembe <jonathanluembe@yahoo.com>
2023-06-20 00:07:16 +0200

    calendar (js pas fini)

--- public/templates/management/calendar.php
+++ public/templates/management/calendar.php
@@ -1,70 +1,26 @@
+<?php include '../../templates/componant/header.html' ?>
+<link rel="stylesheet" href="../../css/calendar.css">
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="UTF-8">
-  <title>Calendrier de Réservation</title>
-<link rel="stylesheet" href="../../css/calendar.css">
-</head>
-<body>
-  <h1>Calendrier de Réservation</h1>
-  <table>
-    <thead>
-      <tr>
-        <th>Lun</th>
-        <th>Mar</th>
-        <th>Mer</th>
-        <th>Jeu</th>
-        <th>Ven</th>
-        <th>Sam</th>
-        <th>Dim</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td class="available">1</td>
-        <td>2</td>
-        <td>3</td>
-        <td>4</td>
-        <td>5</td>
-        <td>6</td>
-        <td class="available">7</td>
-      </tr>
-      <tr>
-        <td>8</td>
-        <td class="available">9</td>
-        <td>10</td>
-        <td>11</td>
-        <td>12</td>
-        <td class="available">13</td>
-        <td class="available">14</td>
-      </tr>
-      <tr>
-        <td>15</td>
-        <td class="available">16</td>
-        <td>17</td>
-        <td>18</td>
-        <td>19</td>
-        <td class="available">20</td>
-        <td class="available">21</td>
-      </tr>
-      <tr>
-        <td>22</td>
-        <td class="available">23</td>
-        <td>24</td>
-        <td>25</td>
-        <td>26</td>
-        <td class="available">27</td>
-        <td class="available">28</td>
-      </tr>
-      <tr>
-        <td>29</td>
-        <td class="available">30</td>
-        <td>31</td>
-      </tr>
-    
-    </tbody>
-  </table>
-
-  <script src="../../js/calendar.js"></script>
+    <div class="containerCalendar">
+        <title>Calendrier de réservation</title>
+        <body>
+            <h1>Sélectionnez une date de réservation</h1>
+            
+            <div id="calendar">
+                <div id="calendar-header">
+                    <button id="prevButton">&lt;</button>
+                    <h2 id="monthYear"></h2>
+                    <button id="nextButton">&gt;</button>
+                </div>
+                <div id="calendar-grid"></div>
+            </div>
+            
+            <button class="goldenButton">Réserver</button>
+        </div>
+  
+    <script src="../../js/calendar.js"></script>
 </body>
+
 </html>
